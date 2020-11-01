4b1fac70784267ef4d705b3ba1d7014935153681
HongSik Lee <uiopppoiu@gmail.com>
2020-11-02 01:54:25 +0900

    Debounce - In ISR

--- main.ino
+++ main.ino
@@ -1,5 +1,7 @@
 #include <cstdio>
 
+const uint32_t debounceTime = 100; // 0.1s
+
 void setupSerial()
 {
     Serial.begin(115200);
@@ -13,14 +15,16 @@ void setupLED()
     Serial.println("LED OK...");
 }
 
-uint32_t lastFallD3 = 0;
+uint32_t lastCallTimeD3 = 0;
 ICACHE_RAM_ATTR void interruptHandler_D3_button()
 {
-    lastFallD3 = millis();
-    // static int count = 0;
-    // char s[64] = {0, };
-    // snprintf(s, sizeof(s), "D3 Button. Count:%d", ++count);
-    // Serial.println(s);
+    if(millis() - lastCallTimeD3 < debounceTime) return;
+    lastCallTimeD3 = millis();
+
+    static int count = 0;
+    char s[64] = {0, };
+    snprintf(s, sizeof(s), "D3 Button. Count:%d", ++count);
+    Serial.println(s);
 }
 
 void setupButtonD3()
@@ -31,14 +35,16 @@ void setupButtonD3()
     Serial.println("D3 Button OK...");
 }
 
-uint32_t lastFallD6 = 0;
+uint32_t lastCallTimeD6 = 0;
 ICACHE_RAM_ATTR void interruptHandler_D6_button()
 {
-    lastFallD6 = millis();
-    // static int count = 0;
-    // char s[64] = {0, };
-    // snprintf(s, sizeof(s), "D6 Button. Count:%d", ++count);
-    // Serial.println(s);
+    if(millis() - lastCallTimeD6 < debounceTime) return;
+    lastCallTimeD6 = millis();
+
+    static int count = 0;
+    char s[64] = {0, };
+    snprintf(s, sizeof(s), "D6 Button. Count:%d", ++count);
+    Serial.println(s);
 }
 
 void setupButtonD6()
@@ -49,14 +55,16 @@ void setupButtonD6()
     Serial.println("D6 Button OK...");
 }
 
-uint32_t lastFallD7 = 0;
+uint32_t lastCallTimeD7 = 0;
 ICACHE_RAM_ATTR void interruptHandler_D7_button()
 {
-    lastFallD7 = millis();
-    // static int count = 0;
-    // char s[64] = {0, };
-    // snprintf(s, sizeof(s), "D7 Button. Count:%d", ++count);
-    // Serial.println(s);
+    if(millis() - lastCallTimeD7 < debounceTime) return;
+    lastCallTimeD7 = millis();
+
+    static int count = 0;
+    char s[64] = {0, };
+    snprintf(s, sizeof(s), "D7 Button. Count:%d", ++count);
+    Serial.println(s);
 }
 
 void setupButtonD7()
@@ -67,47 +75,45 @@ void setupButtonD7()
     Serial.println("D7 Button OK...");
 }
 
+uint32_t lastCallTimeD8 = 0;
+ICACHE_RAM_ATTR void interruptHandler_D8_button()
+{
+    if(millis() - lastCallTimeD8 < debounceTime) return;
+    lastCallTimeD8 = millis();
+
+    static int count = 0;
+    char s[64] = {0, };
+    snprintf(s, sizeof(s), "D8 Button. Count:%d", ++count);
+    Serial.println(s);
+}
+
+void setupButtonD8()
+{
+    const int interrupt_pin = 15; // D8
+    pinMode(interrupt_pin, INPUT_PULLUP);
+    attachInterrupt(interrupt_pin, interruptHandler_D8_button, FALLING);
+    Serial.println("D8 Button OK...");
+}
+
 void setup()
 {
     setupSerial();
+
     setupLED();
-    setupButtonD3();
+    
+
+    // setupButtonD3();
     setupButtonD6();
     setupButtonD7();
+    setupButtonD8();
 
     Serial.println("Initialize done...");
     Serial.println("Initialize done...");
     Serial.println("Initialize done...");
 }
 
-int countD3 = 0;
-int countD6 = 0;
-int countD7 = 0;
 void loop()
 {
     const int loopDelaySec = 500;
     delay(loopDelaySec);
-
-    uint32_t currentTime = millis();
-
-    if (currentTime - lastFallD3 < loopDelaySec)
-    {
-        char s[64] = {0, };
-        snprintf(s, sizeof(s), "D3 Button. Count:%d", ++countD3);
-        Serial.println(s);
-    }
-
-    if (currentTime - lastFallD6 < loopDelaySec)
-    {
-        char s[64] = {0, };
-        snprintf(s, sizeof(s), "D6 Button. Count:%d", ++countD6);
-        Serial.println(s);
-    }
-
-    if (currentTime - lastFallD7 < loopDelaySec)
-    {
-        char s[64] = {0, };
-        snprintf(s, sizeof(s), "D7 Button. Count:%d", ++countD7);
-        Serial.println(s);
-    }
 }
