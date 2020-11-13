6cb46b386eb92cfda2a164c90ff15786897e252f
HongSik Lee <uiopppoiu@gmail.com>
2020-11-13 23:54:38 +0900

    Change not to use functional

--- src/UartInterface/UartMessageCallbackManagement.h
+++ src/UartInterface/UartMessageCallbackManagement.h
@@ -3,7 +3,6 @@
 
 #include <string>
 #include <ctime>
-#include <functional>
 #include <map>
 #include "tinyxml2/tinyxml2.h"
 #include "UartMessageInterface.h"
@@ -16,10 +15,10 @@ namespace UartMessageInterface
     class UartMessageCallbackManagement
     {
     public:
-        typedef std::function<void(eDataType, const std::string &)> CallBackRequestGet;
-        typedef std::function<void(eDataType, const std::string &, const Value &)> CallBackResponseGet;
-        typedef std::function<void(eDataType, const std::string &, uint32_t)> CallBackSubscribe;
-        typedef std::function<void(eDataType, const std::string &)> CallBackUnsubscribe;
+        typedef void(*CallBackRequestGet)(eDataType, const std::string &);
+        typedef void(*CallBackResponseGet)(eDataType, const std::string &, const Value &);
+        typedef void(*CallBackSubscribe)(eDataType, const std::string &, uint32_t);
+        typedef void(*CallBackUnsubscribe)(eDataType, const std::string &);
 
         static void registerRequestGetCallBack(eDataType type, const std::string &name, const CallBackRequestGet &func);
         static void invokeRequestGetCallBack(eDataType type, const std::string &name);
