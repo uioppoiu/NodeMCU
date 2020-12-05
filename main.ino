c3318f1c28df6b766d9f3802b6027fd5bf429ad1
uioppoiu <hongsik.lee86@gmail.com>
2020-12-05 17:10:03 +0900

    .

--- main.ino
+++ main.ino
@@ -77,6 +77,7 @@ void onResponseGet(uint32_t seqId, const UartMessageInterface::ResponseGetData* 
         // dbMsg += String(data.value);
     }
 
+
     Serial.print("URL:");
     Serial.println(dbMsg);
 
